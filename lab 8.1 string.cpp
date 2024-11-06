69f046e1a7cf09f09c350ea0ad4b4fad98aa8678
kondratvlad <vladkondrat87@gmail.com>
2024-11-06 11:39:22 +0200

    first master commit

---
+++ lab 8.1 string.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// Функція для підрахунку груп з трьох знаків оклику "!!!"
+bool ContainsTripleExclamationCpp(const string& str) {
+    return str.find("!!!") != string::npos;
+}
+
+// Функція для заміни трійок знаків оклику "!!!" на "**"
+void ReplaceTripleExclamationCpp(string& str) {
+    size_t pos = 0;
+    while ((pos = str.find("!!!", pos)) != string::npos) {
+        str.replace(pos, 3, "**");
+        pos += 2; // Зсунути на дві позиції далі, щоб уникнути повторної перевірки
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    // Перевірка, чи містить рядок групу з трьох знаків оклику "!!!"
+    bool containsTripleExclamation = ContainsTripleExclamationCpp(str);
+    cout << "Contains triple exclamation: " << (containsTripleExclamation ? "Yes" : "No") << endl;
+
+    // Заміна трійок знаків оклику "!!!" на "**"
+    ReplaceTripleExclamationCpp(str);
+    cout << "Modified string: " << str << endl;
+
+    return 0;
+}
